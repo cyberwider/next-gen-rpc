0cdd233ebf0798f244bca413d22a8607058dfaae
cyberwider <cyberwider@gmail.com>
2023-11-18 06:05:12 +0000

    WIP add HTTP handler

--- src/handler/http.ts
+++ src/handler/http.ts
@@ -1,6 +1,19 @@
 import {corsHeaders} from "../utils";
 import {RequestContext} from "../types";
 
+async function makeRequestToServer(
+    url: URL,
+    body: any,
+    method: string,
+    headers: Headers,
+): Promise<Response> {
+    return await fetch(url, {
+        // fetch raises "TypeError:Request with a GET or HEAD method cannot have a body"
+        body: ['GET', 'HEAD'].includes(method) ? null : body,
+        method: method,
+        headers: headers,
+    });
+}
 
 async function handleHttpRequest(context: RequestContext): Promise<void> {
     const {request, config} = context;
@@ -19,17 +32,6 @@ async function handleHttpRequest(context: RequestContext): Promise<void> {
         try {
             const url: URL = new URL(origin.httpEndpoint);
             serverResponse = await makeRequestToServer(url, requestBody, request.method, request.headers);
-            if (serverResponse.status >= 500) {
-                console.log(`failed to fetch ${origin.slug} exception`);
-            } else {
-                context.response = new Response(serverResponse.body, {
-                    status: serverResponse.status,
-                    statusText: serverResponse.statusText,
-                    headers: corsHeaders,
-                });
-                context.response.headers.set('x-origin-slug', origin.slug);
-                break;
-            }
         } catch (e) {
             console.log(e);
             break;
